37aa3b7a221a970e16c39e80c4ef3dc304f6b7a7
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2023-09-20 18:01:26 +0900

    test

--- app/controllers/foo/bars_controller.rb
+++ app/controllers/foo/bars_controller.rb
@@ -1,9 +1,9 @@
 class BarsController < ApplicationController
-  # こんにちは
   def show
     @instance_text = params[:bar]
     @instance_text2 = params[:bar2]
     @instance_text3 = params[:bar3]
     @instance_text4 = params[:bar4]
+    @instance_text5 = params[:bar5]
   end
 end
